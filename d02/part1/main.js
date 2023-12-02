995ab19c28881039b97961d8efb807f12e628615
senari <onextthenitromancer@gmail.com>
2023-12-02 17:03:09 +0100

    Day 2 solution in JS (node).

---
+++ d02/part1/main.js
@@ -0,0 +1,37 @@
+const fs = require("node:fs");
+
+fs.readFile("../input.txt", "ascii", (err, data) => {
+	if (err)
+		return console.error("Failed to read input file :(\n", err);
+
+	let ret = 0;
+
+	const getHighest = (line, regex) => {
+		let highest = 0;
+		const matches = line.match(regex);
+
+		if (matches !== null)
+			matches.forEach((entry) => {
+				const temp = parseInt(entry);
+				
+				if (temp > highest)
+					highest = temp;
+			});
+		return highest;
+	};
+
+	const lines = data.split("\n");
+	lines.forEach((line, i) => {
+		if (line.length < 7) // If line's empty don't count it.
+			return;
+		let r = g = b = 0;
+		r = getHighest(line, / \d* red/g);
+		g = getHighest(line, / \d* green/g);
+		b = getHighest(line, / \d* blue/g);
+
+		if (r <= 12 && g <= 13 && b <= 14)
+			ret += i + 1;
+	});
+
+	console.log("Total sum of IDs is:", ret);
+});
